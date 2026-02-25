faee64b20333ec3b37c3758b2ca2f31e1ffd9350
Embers-of-the-Fire <stellarishs@163.com>
2026-02-25 19:02:41 +0800

    fix: add flake support and fix error code

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "CUDA dev environment";
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells.${system}.default =
+        (pkgs.buildFHSEnv {
+          name = "nvidia-cuda-dev";
+          targetPkgs =
+            pkgs:
+            (with pkgs; [
+              zlib
+
+              cudatoolkit
+              linuxPackages.nvidia_x11
+              libGLU
+              libGL
+              xorg.libXi
+              xorg.libXmu
+              xorg.libXext
+              xorg.libX11
+              xorg.libXv
+              xorg.libXrandr
+
+              stdenv.cc
+              binutils
+
+              fish
+            ]);
+
+          profile = ''
+            export LD_LIBRARY_PATH="${pkgs.linuxPackages.nvidia_x11}/lib"
+            export CUDA_PATH="${pkgs.cudatoolkit}"
+            export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+            export EXTRA_CCFLAGS="-I/usr/include"
+          '';
+
+          runScript = "fish";
+        }).env;
+    };
+}
